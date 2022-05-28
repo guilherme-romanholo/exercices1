b31ed2739ae883abfe2fe58f12d2b46722b2dd5c
Guilherme Romanholo Bofo <89668419+guilherme-romanholo@users.noreply.github.com>
2022-05-28 18:21:17 -0300

    Update ex01.c

--- ex01.c
+++ ex01.c
@@ -1,3 +1,16 @@
+/*
+Escreva um programa para controlar o estoque de uma loja que trabalha com cinco tipos de
+produtos. O algoritmo deverá permitir que o usuário escolha uma das seguintes opções:
+Aumentar estoque
+Baixar estoque
+Visualizar estoque de um produto
+Visualizar estoque total
+Aumentar preço
+Diminuir preço
+Trocar produto
+Sair
+*/
+
 #include <stdio.h>
 
 int main(int argc, char const *argv[])
